2cc1d0038be50389ef4a72425f7f799303c29454
Ben Krepp <bkrepp@ctps.org>
2023-10-20 16:22:50 -0400

    Cell coordinates according to Casey's spreadsheet #2 of 10/19/23

--- prototype1.py
+++ prototype1.py
@@ -13,20 +13,22 @@ date_coords = 'C2'
 temperature_coords = 'C3'
 sky_coords = 'C4'
 #
-# loc_id_coords = 'TBD'
-loc_type_coords = 'D10'
+bp_loc_id_coords = 'H5'	 # This is a hidden cell in the spreadsheet
 loc_desc_coords = 'D6'
 loc_desc_other_coords = 'F6'
-#
-fac_name_coords = 'D7'
+fac_name_coords = 'D6'
 fac_name_other_coords = 'F7'
 #
-street_1_coords = 'D12'
-street_2_coords = 'D13'
+street_1_coords = 'D7'
 street_1_dir_coords = 'D8'
-street_2_dir_coords = 'D9'
+street_2_coords = 'D9'
+street_2_dir_coords = 'D10'
 #
-comments_coords = 'I4'
+loc_type_coords = 'D11'
+muni_coords = 'D12'
+muni_other_coords = 'F12'
+#
+comments_coords = 'J4'
 
 # Pseudo-constants for 'indices' of count-types in the count sheets.
 bike_col = 'B'
@@ -65,17 +67,26 @@ def initialize(input_fn):
 
 def read_overview_sheet():
 	global overview_sheet, debug
-    
+	
+	bp_loc_id = overview_sheet[bp_loc_id_coords].value
 	date_raw = overview_sheet[date_coords].value
-	loc_type = overview_sheet[loc_type_coords].value
+	
 	loc_desc = overview_sheet[loc_desc_coords].value
-    if loc_desc == 'Other':
-        loc_desc = overview_sheet[loc_desc_other_coords].value
-    #
+	if loc_desc == 'Other':
+		loc_desc = overview_sheet[loc_desc_other_coords].value
+	#
+	loc_type = overview_sheet[loc_type_coords].value
+	
+	muni = overview_sheet[muni_coords].value
+	if muni == 'Other':
+		muni = overview_sheet[muni_other_coords].value
+	#	 
+	
 	fac_name = overview_sheet[fac_name_coords].value
-    if fac_name == 'Other':
-        fac_name = overview_sheet[fac_name_other_coords].value
-    #
+	if fac_name == 'Other':
+		fac_name = overview_sheet[fac_name_other_coords].value
+	#
+	
 	street_1 = overview_sheet[street_1_coords].value
 	street_1_dir = overview_sheet[street_1_dir_coords].value:
 	street_2 = overview_sheet[street_2_coords].value
@@ -84,11 +95,13 @@ def read_overview_sheet():
 	temperature = overview_sheet[temperature_coords].value
 	sky = overview_sheet[sky_coords].value
 	comments = overview_sheet[comments_coords].value
-    
+	
 	if debug == True:
-		print('date ' + str(date_raw))
-		print('location type = ' + loc_type
+		print('bp_loc_id = ' + bp_loc_id)
+		print('date	 =' + str(date_raw))
 		print('location description = ' + loc_desc)
+		print('location type = ' + loc_type
+		print('municipality = ' + muni)
 		print('facility name = ' + fac_name)
 		print('street 1 = ' + street_1)
 		print('street 1 direction = ' + street_1_dir)
@@ -97,20 +110,18 @@ def read_overview_sheet():
 		print('temperature = ' + str(temperature))
 		print('sky = ' + sky)
 		print('comments = '	 + comments)
-    # end_if 
-		 
-    # *** TODO: Collect count location ID
-    bp_loc_id = 42
-    # *** TODO: Clean up raw date
-    date_cooked = date_raw
-    
-    # Assemble return value: dict of information harvested from overview table
-    retval = { 'bp_loc_id' : bp_loc_id, 'date' : date_cooked, 
-               'street_1' : street_1, 'street_1_dir' : street_1_dir,
-               'street_2' : street_2, 'street_2_dir' : street_2_dir,   
-               'temperature' : temperature, 'sky' : sky,
-               'comments' : comments }
-    return retval
+	# end_if 
+		
+	# *** TODO: Clean up raw date
+	date_cooked = date_raw
+	
+	# Assemble return value: dict of information harvested from overview table
+	retval = { 'bp_loc_id' : bp_loc_id, 'date' : date_cooked, 
+			   'street_1' : street_1, 'street_1_dir' : street_1_dir,
+			   'street_2' : street_2, 'street_2_dir' : street_2_dir,   
+			   'temperature' : temperature, 'sky' : sky,
+			   'comments' : comments }
+	return retval
 # end_def: read_overview_sheet
 
 # Read data from one count sheet.
@@ -250,7 +261,7 @@ def read_count_sheet(count_sheet, rows):
 	# Assemble return value: dict of list of each count type
 	retval = { 'bike' : bike_temp, 'ped': ped_temp, 'child' : child_temp,
 			   'jogger' : jogger_temp, 'skater' : skater_temp,
-               'wheelchair' : wheelchair_temp, 'other' : other_temp }
+			   'wheelchair' : wheelchair_temp, 'other' : other_temp }
 	return retval
 # end_def: read_count sheet
 
@@ -263,25 +274,25 @@ def read_count_tabs():
 	s3_data = read_count_sheet(count_sheet_3, sheet_3_rows)
 	s4_data = read_count_sheet(count_sheet_4, sheet_4_rows)
 	s5_data = read_count_sheet(count_sheet_5, sheet_5_rows)
-    # Assemble count data from all sheets
+	# Assemble count data from all sheets
 	bike_data = s1['bike'] + s2['bike'] + s3['bike'] + s4['bike'] + s5['bike']
 	ped_data = s1['ped']+ s2['ped'] + s3['ped'] + s4['ped'] + s5['ped']
 	child_data = s1['child']+ s2['child'] + s3['child'] + s4['child'] + s5['child']
 	jogger_data = s1['jogger']+ s2['jogger'] + s3['jogger'] + s4['jogger'] + s5['jogger']
 	skater_data = s1['skater']+ s2['skater'] + s3['skater'] + s4['skater'] + s5['skater']
 	wheelchair_data = s1['skater']+ s2['skater'] + s3['skater'] + s4['skater'] + s5['skater']
-	other_data = s1['other']+ s2['other'] + s3['other'] + s4['other'] + s5['other']  
-     # Assemble return value
-    retval = { 'bike' : bike_data, 'ped' : ped_data, 'child' : child_data,
-               'jogger' : jogger_data, 'skater' : skater_data,
-               'wheelchair' : wheelchair_data, 'other' : other_data }
-    return retval   
+	other_data = s1['other']+ s2['other'] + s3['other'] + s4['other'] + s5['other']	 
+	 # Assemble return value
+	retval = { 'bike' : bike_data, 'ped' : ped_data, 'child' : child_data,
+			   'jogger' : jogger_data, 'skater' : skater_data,
+			   'wheelchair' : wheelchair_data, 'other' : other_data }
+	return retval	
 # end_def: read_count_tabs
 
 # Test uber-driver routine:
 def test_driver():
 	initialize(input_xlsx_fn)
-    overview_data = read_overview_tab()
+	overview_data = read_overview_tab()
 	count_data = read_count_sheets()
-    # Here: Have all info needed to assemble and run SQL INSERT query
+	# Here: Have all info needed to assemble and run SQL INSERT query
 # end_def: test_driver
