01960e7ba3bff3eb5b25076579e6702f2d172595
Ben Krepp <bkrepp@ctps.org>
2023-10-27 16:31:52 -0400

    street_1 -> from_st_name, etc; quote string VALUES; 'count_date' VALUE TBD

--- sql/sample_query_1.sql
+++ sql/sample_query_1.sql
@@ -1,10 +1,9 @@
-INSERT INTO bp_counts_staging (bp_loc_id, count_id, count_date, count_type, street_1_dir, street_2_dir, sky,
-            cnt_0000, cnt_0245, cnt_0300, cnt_0315, cnt_0330, cnt_0345, cnt_0400, cnt_0415, cnt_0430, cnt_0445, cnt_0500, cnt_0515, cnt_0530, cnt_0545, 
-			cnt_0600, cnt_0615, cnt_0630, cnt_0645, cnt_0700, cnt_0730, cnt_0745, cnt_0800, cnt_0815, cnt_0830, cnt_0845, cnt_0900, cnt_0915, cnt_0930, 
-			cnt_0945, cnt_1000, cnt_1015, cnt_1030, cnt_1045, cnt_1100, cnt_1115, cnt_1130, cnt_1145, cnt_1200, cnt_1215, cnt_1230, cnt_1245, cnt_1300, 
-			cnt_1315, cnt_1330, cnt_1345, cnt_1400, cnt_1415, cnt_1430, cnt_1445, cnt_1500, cnt_1515, cnt_1530, cnt_1545, cnt_1600, cnt_1615, cnt_1630, 
-			cnt_1645, cnt_1700, cnt_1715, cnt_1730, cnt_1745, cnt_1800, cnt_1815, cnt_1830, cnt_1845, cnt_1900, cnt_1915, cnt_1930, cnt_1945, cnt_2000, 
-			cnt_2015, cnt_2030, cnt_2045, cnt_2100 ) 
-VALUES 		( 98765, 12345, 10/23/2023, 'B', 'All', 'All', 
-              1, 1, 1, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 1, 11, 22, 33, 44, 66, 77, 88, 99, 99, 88, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 12,
-			  13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1 );
+INSERT INTO bp_counts_staging (bp_loc_id, count_id, count_date, count_type, from_st_dir, to_st_dir, sky,  
+                               cnt_0000, cnt_0245, cnt_0300, cnt_0315, cnt_0330, cnt_0345, cnt_0400, cnt_0415, cnt_0430, cnt_0445, cnt_0500, cnt_0515, cnt_0530, cnt_0545,  
+							   cnt_0600, cnt_0615, cnt_0630, cnt_0645, cnt_0700, cnt_0730, cnt_0745, cnt_0800, cnt_0815, cnt_0830, cnt_0845, cnt_0900, cnt_0915, cnt_0930, 
+							   cnt_0945, cnt_1000, cnt_1015, cnt_1030, cnt_1045, cnt_1100, cnt_1115, cnt_1130, cnt_1145, cnt_1200, cnt_1215, cnt_1230, cnt_1245, cnt_1300, 
+							   cnt_1315, cnt_1330, cnt_1345, cnt_1400, cnt_1415, cnt_1430, cnt_1445, cnt_1500, cnt_1515, cnt_1530, cnt_1545, cnt_1600, cnt_1615, cnt_1630, 
+							   cnt_1645, cnt_1700, cnt_1715, cnt_1730, cnt_1745, cnt_1800, cnt_1815, cnt_1830, cnt_1845, cnt_1900, cnt_1915, cnt_1930, cnt_1945, cnt_2000, 
+							   cnt_2015, cnt_2030, cnt_2045, cnt_2100 ) 
+VALUES 						( 98765, 12345, 10/23/2023, 'B', 'All', 'All', 1, 1, 1, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 1, 11, 22, 33, 44, 66, 77, 88, 99, 99, 88,
+ 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1 );
